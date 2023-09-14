f6ddc1c5966061871d06d6b76a671b888acd7876
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2023-09-14 15:48:30 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
 
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+
+const bodyColor = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+console.log(btnStart);
+const btnStop = document.querySelector('[data-stop]');
+console.log(btnStop);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    bodyColor.style.backgroundColor = randomColor;
+    }, 1000);
+
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+});
+
+
+
+
+
+      
